b199aa0ac313594ac9e4f58c99531d5abe4ddb16
Lara <lara.durrant@gmail.com>
2019-05-12 09:50:43 -0700

    Initialize repository

---
+++ Story Game/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested for " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit game request received.");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
